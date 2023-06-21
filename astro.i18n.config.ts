47344b6bd5dd6e955c60bdff79f400ad176624b5
Nicolas Renault <59968905+NicolasRenault@users.noreply.github.com>
2023-06-21 11:10:24 +0200

    Update degree intro

--- astro.i18n.config.ts
+++ astro.i18n.config.ts
@@ -23,7 +23,7 @@ export default defineAstroI18nConfig({
 				hello: "Hi, I'm",
 				intro_1: "I'm a",
 				intro_2:
-					"Developer based in France. I'm graduated from a Three-year Degree in web and mobile device software technology, with the last year in block release training. I am currently working at Trelleborg as an IT Developer and Software Administrator.",
+					"Developer based in France. I'm graduated from a Bachelor's Degree in web and mobile. I am currently working at Trelleborg as an IT Developer and Software Administrator.",
 				working_experience: "Working experience",
 				fullstack_php_developer: "Fullstack PHP Developer",
 				selected_projects: "Selected projects",
@@ -59,7 +59,7 @@ export default defineAstroI18nConfig({
 				hello: "Salut, je suis",
 				intro_1: "Je suis un Développeur",
 				intro_2:
-					"basé en France. Je suis  Diplômé d'un DUT informatique et d'une licence professionnelle des technologies logicielles du web et terminaux mobiles en alternance. Je travaille actuellement chez Trelleborg en tant que développeur informatique et administrateur de logiciels.",
+					"basé en France. Je suis Diplômé d'un DUT informatique et d'une Licence professionnelle des technologies logicielles du web et terminaux mobiles en alternance. Je travaille actuellement chez Trelleborg en tant que développeur informatique et administrateur de logiciels.",
 				working_experience: "Expérience professionnelle",
 				fullstack_php_developer: "Développeur PHP Fullstack",
 				selected_projects: "Projets sélectionnés",
