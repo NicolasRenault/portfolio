259bcfe68ce01d5a51da2c34b5e2f478e1d0758e
Nicolas Renault <contact.nicolasrenault@gmail.com>
2023-08-02 12:04:35 +0200

    Fix astro config for scopedStyleStrategy for dev environment. See: https://github.com/withastro/astro/issues/6975#issuecomment-1587710853

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import i18n from "astro-i18n"
-import { defineConfig } from 'astro/config';
+import i18n from "astro-i18n";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({integrations: [i18n()],});
+export default defineConfig({ integrations: [i18n()], scopedStyleStrategy: "class" });
