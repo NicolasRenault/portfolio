8059969f516e6db9c6c4b1ee5bbd7b5d2f5120eb
Nicolas Renault <contact.nicolasrenault@gmail.com>
2023-09-01 19:52:18 +0200

    Upgrade to Astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@ import i18n from "astro-i18n";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ integrations: [i18n()], scopedStyleStrategy: "class" });
+export default defineConfig({
+	integrations: [i18n()],
+	scopedStyleStrategy: "class",
+	build: {
+		inlineStylesheets: "never",
+	},
+});
